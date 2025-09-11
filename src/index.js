268dece78fb5fcc4f7935b4fb9e565ca7c64be94
angnat <angnat05@yahoo.com>
2025-09-11 22:18:12 +0700

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
+import mongoose from 'mongoose';
 import app from './app.js'
+import logger from './configs/logger.config.js';
 
 //env variable
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
+
+//exit on mognodb error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongodb connection error : ${err}`);
+  process.exit(1);
+});
+
+//mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+//mongodb connection
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to Mongodb.");
+  });
 
 app.listen(PORT, ()=> {
     console.log("Server is listening at ", PORT);
